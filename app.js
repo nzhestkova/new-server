0623b8d852ab68fa434e43846d2e978b4db67fbe
natalia <zhestckova.natali@yandex.ru>
2020-04-09 14:52:46 +0300

    404 error modified

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.set('view engine', 'pug');
 
 app.use(cors());
 app.options("*", cors());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
