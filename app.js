27b2c7ecdc3a024fa5268790419e280d9fa8937c
natalia <zhestckova.natali@yandex.ru>
2020-06-24 15:37:45 +0300

    ~ latest server version #3

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.options("*", cors());
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
